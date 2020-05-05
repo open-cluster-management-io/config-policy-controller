e8acd2ca556e434438101f91ae419c2f6be236b8
Will <willkutler@gmail.com>
2020-05-05 14:43:43 -0400

    use new make, travis, /build files

---
+++ build/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+echo "BUILD GOES HERE!"
+
+echo "<repo>/<component>:<tag> : $1"
+
+# Run our build target and set IMAGE_NAME_AND_VERSION
+export IMAGE_NAME_AND_VERSION=${1}
+make build
+make build-images
---
+++ build/run-e2e-tests.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+
+echo "E2E TESTS GO HERE!"
+
+exit 0;
