1b58ac7b3f523f0063fad8ec50dc329fc8ca192e
William Kutler <57921170+willkutler@users.noreply.github.com>
2021-10-11 16:02:03 -0400

    store api resource list in case of failure (#170)
    * store api resource list in case of failure
    
    Signed-off-by: Will Kutler <wkutler@redhat.com>
    
    * fmt
    
    Signed-off-by: Will Kutler <wkutler@redhat.com>
    
    * ignore error if partial list is returned
    
    Signed-off-by: Will Kutler <wkutler@redhat.com>

--- pkg/controller/configurationpolicy/configurationpolicy_controller.go
+++ pkg/controller/configurationpolicy/configurationpolicy_controller.go
@@ -187,6 +187,8 @@ func (r *ReconcileConfigurationPolicy) Reconcile(request reconcile.Request) (rec
 // PeriodicallyExecConfigPolicies loops through all configurationpolicies in the target namespace and triggers
 // template handling for each one. This function drives all the work the configuration policy controller does.
 func PeriodicallyExecConfigPolicies(freq uint, test bool) {
+	cachedApiResourceList := []*metav1.APIResourceList{}
+	cachedApiGroupsList := []*restmapper.APIGroupResources{}
 	for {
 		start := time.Now()
 		printMap(availablePolicies.PolicyMap)
@@ -203,14 +205,25 @@ func PeriodicallyExecConfigPolicies(freq uint, test bool) {
 		//get resources once per cycle to avoid hanging
 		dd := clientSet.Discovery()
 		apiresourcelist, apiresourcelistErr := dd.ServerResources()
+		if len(apiresourcelist) > 0 {
+			cachedApiResourceList = append([]*metav1.APIResourceList{}, apiresourcelist...)
+		}
 		skipLoop := false
-		if apiresourcelistErr != nil {
+		if apiresourcelistErr != nil && len(cachedApiResourceList) > 0 {
+			glog.Errorf("Error getting API resource list. Using cached list...")
+			apiresourcelist = cachedApiResourceList
+		} else if apiresourcelistErr != nil {
 			skipLoop = true
 			glog.Errorf("Failed to retrieve apiresourcelist with err: %v", apiresourcelistErr)
 		}
 		apigroups, apigroupsErr := restmapper.GetAPIGroupResources(dd)
-
-		if !skipLoop && apigroupsErr != nil {
+		if len(apigroups) > 0 {
+			cachedApiGroupsList = append([]*restmapper.APIGroupResources{}, apigroups...)
+		}
+		if apigroupsErr != nil && len(cachedApiGroupsList) > 0 {
+			glog.Errorf("Error getting API groups list. Using cached list...")
+			apigroups = cachedApiGroupsList
+		} else if !skipLoop && apigroupsErr != nil {
 			skipLoop = true
 			glog.Errorf("Failed to retrieve apigroups with err: %v", apigroupsErr)
 
