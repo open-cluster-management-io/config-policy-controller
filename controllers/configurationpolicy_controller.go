e95d620ac77d912e4bf3e093c68dfbbf7fda478b
Will Kutler <wkutler@redhat.com>
2022-09-27 14:48:40 -0400

    add metrics to time the compare algorithm
    Signed-off-by: Will Kutler <wkutler@redhat.com>
    (cherry picked from commit f9814ef6c52111491dd807a057894228b44ad57a)

--- controllers/configurationpolicy_controller.go
+++ controllers/configurationpolicy_controller.go
@@ -73,6 +73,62 @@ var (
 	reasonCleanupError       = "Error cleaning up child objects"
 )
 
+<<<<<<< HEAD
+=======
+var evalLoopHistogram = prometheus.NewHistogram(
+	prometheus.HistogramOpts{
+		Name:    "config_policies_evaluation_duration_seconds",
+		Help:    "The seconds that it takes to evaluate all configuration policies on the cluster",
+		Buckets: []float64{1, 3, 9, 10.5, 15, 30, 60, 90, 120, 180, 300, 450, 600},
+	},
+)
+
+var policyEvalSecondsCounter = prometheus.NewCounterVec(
+	prometheus.CounterOpts{
+		Name: "config_policy_evaluation_seconds_total",
+		Help: "The total seconds taken while evaluating the configuration policy. Use this alongside " +
+			"config_policy_evaluation_total.",
+	},
+	[]string{"name"},
+)
+
+var policyEvalCounter = prometheus.NewCounterVec(
+	prometheus.CounterOpts{
+		Name: "config_policy_evaluation_total",
+		Help: "The total number of evaluations of the configuration policy. Use this alongside " +
+			"config_policy_evaluation_seconds_total.",
+	},
+	[]string{"name"},
+)
+
+var compareObjSecondsCounter = prometheus.NewCounterVec(
+	prometheus.CounterOpts{
+		Name: "compare_objects_seconds_total",
+		Help: "The total seconds taken while comparing policy objects. Use this alongside " +
+			"compare_objects_evaluation_total.",
+	},
+	[]string{"config_policy_name", "object"},
+)
+
+var compareObjEvalCounter = prometheus.NewCounterVec(
+	prometheus.CounterOpts{
+		Name: "compare_objects_evaluation_total",
+		Help: "The total number of times the comparison algorithm is run on an object. " +
+			"Use this alongside compare_objects_seconds_total.",
+	},
+	[]string{"config_policy_name", "object"},
+)
+
+func init() {
+	// Register custom metrics with the global Prometheus registry
+	metrics.Registry.MustRegister(evalLoopHistogram)
+	metrics.Registry.MustRegister(policyEvalSecondsCounter)
+	metrics.Registry.MustRegister(policyEvalCounter)
+	metrics.Registry.MustRegister(compareObjSecondsCounter)
+	metrics.Registry.MustRegister(compareObjEvalCounter)
+}
+
+>>>>>>> 19f845c (add metrics to time the compare algorithm)
 // SetupWithManager sets up the controller with the Manager.
 func (r *ConfigurationPolicyReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
@@ -134,6 +190,7 @@ func (r *ConfigurationPolicyReconciler) Reconcile(ctx context.Context, request c
 		// If the metric was not deleted, that means the policy was never evaluated so it can be ignored.
 		_ = policyEvalSecondsCounter.DeleteLabelValues(request.Name)
 		_ = policyEvalCounter.DeleteLabelValues(request.Name)
+<<<<<<< HEAD
 		_ = plcTempsProcessSecondsCounter.DeleteLabelValues(request.Name)
 		_ = plcTempsProcessCounter.DeleteLabelValues(request.Name)
 		_ = compareObjEvalCounter.DeletePartialMatch(prometheus.Labels{"config_policy_name": request.Name})
@@ -142,6 +199,10 @@ func (r *ConfigurationPolicyReconciler) Reconcile(ctx context.Context, request c
 			prometheus.Labels{"policy": fmt.Sprintf("%s/%s", request.Namespace, request.Name)})
 		_ = policyUserErrorsCounter.DeletePartialMatch(prometheus.Labels{"template": request.Name})
 		_ = policySystemErrorsCounter.DeletePartialMatch(prometheus.Labels{"template": request.Name})
+=======
+		_ = compareObjEvalCounter.DeletePartialMatch(prometheus.Labels{"config_policy_name": request.Name})
+		_ = compareObjSecondsCounter.DeletePartialMatch(prometheus.Labels{"config_policy_name": request.Name})
+>>>>>>> 19f845c (add metrics to time the compare algorithm)
 	}
 
 	return reconcile.Result{}, nil
@@ -1514,6 +1575,7 @@ func (r *ConfigurationPolicyReconciler) handleSingleObj(
 
 		before := time.Now().UTC()
 
+<<<<<<< HEAD
 		throwSpecViolation, msg, pErr := r.checkAndUpdateResource(obj, compType, mdCompType, remediation)
 
 		duration := time.Now().UTC().Sub(before)
@@ -1526,6 +1588,20 @@ func (r *ConfigurationPolicyReconciler) handleSingleObj(
 		compareObjEvalCounter.WithLabelValues(
 			obj.policy.Name,
 			obj.namespace,
+			fmt.Sprintf("%s.%s", obj.gvr.Resource, obj.name),
+		).Inc()
+=======
+		throwSpecViolation, msg, pErr := r.checkAndUpdateResource(obj, compType, mdCompType, remediation, dclient)
+>>>>>>> 19f845c (add metrics to time the compare algorithm)
+
+		duration := time.Now().UTC().Sub(before)
+		seconds := float64(duration) / float64(time.Second)
+		compareObjSecondsCounter.WithLabelValues(
+			obj.policy.Name,
+			fmt.Sprintf("%s.%s", obj.gvr.Resource, obj.name),
+		).Add(seconds)
+		compareObjEvalCounter.WithLabelValues(
+			obj.policy.Name,
 			fmt.Sprintf("%s.%s", obj.gvr.Resource, obj.name),
 		).Inc()
 
--- go.mod
+++ go.mod
@@ -76,16 +76,25 @@ require (
 	go.uber.org/multierr v1.6.0 // indirect
 	go.uber.org/zap v1.21.0 // indirect
 	golang.org/x/crypto v0.0.0-20220214200702-86341886e292 // indirect
+<<<<<<< HEAD
 	golang.org/x/net v0.0.0-20220722155237-a158d28d115b // indirect
 	golang.org/x/oauth2 v0.0.0-20220223155221-ee480838109b // indirect
 	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
+=======
+	golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
+	golang.org/x/oauth2 v0.0.0-20220223155221-ee480838109b // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+>>>>>>> 19f845c (add metrics to time the compare algorithm)
 	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	golang.org/x/time v0.0.0-20220210224613-90d013bbcef8 // indirect
 	gomodules.xyz/jsonpatch/v2 v2.2.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
+<<<<<<< HEAD
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+=======
+>>>>>>> 19f845c (add metrics to time the compare algorithm)
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
