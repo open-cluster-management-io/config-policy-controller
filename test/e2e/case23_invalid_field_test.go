0263cc19d78d8fad68040498b5841b124b48efc2
clyang82 <chuyang@redhat.com>
2023-05-25 17:51:32 +0800

    change FieldValidation to strict use FieldValidation only if the k8s version is above 1.25
    Signed-off-by: clyang82 <chuyang@redhat.com>

--- test/e2e/case23_invalid_field_test.go
+++ test/e2e/case23_invalid_field_test.go
@@ -8,6 +8,7 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"golang.org/x/mod/semver"
 	corev1 "k8s.io/api/core/v1"
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -23,12 +24,24 @@ var _ = Describe("Test an objectDefinition with an invalid field", Ordered, func
 		policyYAML    = "../resources/case23_invalid_field/policy.yaml"
 	)
 
+	var serverVersion string
+	BeforeAll(func() {
+		serverVersion = utils.GetServerVersion(clientManaged)
+	})
+
 	It("Fails when an invalid field is provided", func() {
 		By("Creating the " + policyName + " policy")
 		utils.Kubectl("apply", "-f", policyYAML, "-n", testNamespace)
 
 		expectedMsg := "configmaps [case23] in namespace default is missing, and cannot be created, reason: " +
-			"`ValidationError(ConfigMap): unknown field \"invalid\" in io.k8s.api.core.v1.ConfigMap`"
+			"`ConfigMap in version \"v1\" cannot be handled as a ConfigMap: strict decoding error: unknown " +
+			"field \"invalid\"`"
+
+		// if the server version is less than 1.25.0, we use client side validation
+		if semver.Compare(serverVersion, "v1.25.0") < 0 {
+			expectedMsg = "configmaps [case23] in namespace default is missing, and cannot be created, reason: " +
+				"`ValidationError(ConfigMap): unknown field \"invalid\" in io.k8s.api.core.v1.ConfigMap`"
+		}
 
 		By("Verifying that the " + policyName + " policy is noncompliant")
 		Eventually(func(g Gomega) {
@@ -43,11 +56,16 @@ var _ = Describe("Test an objectDefinition with an invalid field", Ordered, func
 		By("Verifying events do not continue to be created after the first violation for created objects")
 		startTime := metav1.NewTime(time.Now())
 
+		msg := "ConfigMap in version \"v1\" cannot be handled as a ConfigMap: " +
+			"strict decoding error: unknown field \"invalid\""
+		if semver.Compare(serverVersion, "v1.25.0") < 0 {
+			msg = "unknown field \"invalid\" in io.k8s.api.core.v1.ConfigMap"
+		}
 		Consistently(func() interface{} {
 			compPlcEvents := utils.GetMatchingEvents(clientManaged, testNamespace,
 				policyName,
 				"",
-				"unknown field \"invalid\" in io.k8s.api.core.v1.ConfigMap",
+				msg,
 				defaultTimeoutSeconds)
 
 			if len(compPlcEvents) == 0 {
@@ -66,8 +84,13 @@ var _ = Describe("Test an objectDefinition with an invalid field", Ordered, func
 		_, err := clientManaged.CoreV1().ConfigMaps("default").Create(context.TODO(), configmap, metav1.CreateOptions{})
 		Expect(err).ToNot(HaveOccurred())
 
-		expectedMsg = "Error validating the object case23, the error is `ValidationError(ConfigMap): unknown " +
-			"field \"invalid\" in io.k8s.api.core.v1.ConfigMap`"
+		expectedMsg = "Error validating the object case23, the error is `ConfigMap in version \"v1\"" +
+			" cannot be handled as a ConfigMap: strict decoding error: unknown field \"invalid\"`"
+		// if the server version is less than 1.25.0, we use client side validation
+		if semver.Compare(serverVersion, "v1.25.0") < 0 {
+			expectedMsg = "Error validating the object case23, the error is `ValidationError(ConfigMap): unknown " +
+				"field \"invalid\" in io.k8s.api.core.v1.ConfigMap`"
+		}
 		Eventually(func() interface{} {
 			managedPlc := utils.GetWithTimeout(
 				clientManagedDynamic, gvrConfigPolicy, policyName, testNamespace, true, defaultTimeoutSeconds,
@@ -79,11 +102,15 @@ var _ = Describe("Test an objectDefinition with an invalid field", Ordered, func
 		By("Verifying events do not continue to be created after the first violation for existing objects")
 		alreadyExistsStartTime := metav1.NewTime(time.Now())
 
+		msg = "strict decoding error: unknown field \"invalid\""
+		if semver.Compare(serverVersion, "v1.25.0") < 0 {
+			msg = "unknown field \"invalid\" in io.k8s.api.core.v1.ConfigMap"
+		}
 		Consistently(func() interface{} {
 			compPlcEvents := utils.GetMatchingEvents(clientManaged, testNamespace,
 				policyName,
 				"",
-				"unknown field \"invalid\" in io.k8s.api.core.v1.ConfigMap",
+				msg,
 				defaultTimeoutSeconds)
 
 			if len(compPlcEvents) == 0 {
