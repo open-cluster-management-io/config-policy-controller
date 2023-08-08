207833406ce4725d9b6fc5dbb7d289d2fcd73fce
Justin Kulikauskas <jkulikau@redhat.com>
2023-08-08 13:39:49 -0400

    Set Burst and QPS on target k8s client
    Previously, these settings were not being applied to the config used
    in 'hosted mode'. This commit also includes a typo fix, and adjusts some
    settings used by the tests.
    
    Signed-off-by: Justin Kulikauskas <jkulikau@redhat.com>

--- test/e2e/e2e_suite_test.go
+++ test/e2e/e2e_suite_test.go
@@ -14,6 +14,7 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/format"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -62,6 +63,8 @@ func init() {
 }
 
 var _ = BeforeSuite(func() {
+	format.TruncatedDiff = false
+
 	By("Setup Hub client")
 	gvrPod = schema.GroupVersionResource{Group: "", Version: "v1", Resource: "pods"}
 	gvrNS = schema.GroupVersionResource{Group: "", Version: "v1", Resource: "namespaces"}
