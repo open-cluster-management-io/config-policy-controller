e01b05c9fe68819440d2042fb74ab1e5b5711ee3
Justin Kulikauskas <jkulikau@redhat.com>
2024-09-27 11:57:45 -0400

    Correct details list when templates are removed
    Refs:
     - https://issues.redhat.com/browse/ACM-14550
    
    Signed-off-by: Justin Kulikauskas <jkulikau@redhat.com>

--- test/e2e/case5_multi_test.go
+++ test/e2e/case5_multi_test.go
@@ -6,88 +6,110 @@ package e2e
 import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 
 	"open-cluster-management.io/config-policy-controller/test/utils"
 )
 
-const (
-	case5ConfigPolicyNameInform  string = "policy-pod-multi-mh"
-	case5ConfigPolicyNameEnforce string = "policy-pod-multi-create"
-	case5ConfigPolicyNameCombo   string = "policy-pod-multi-combo"
-	case5PodName1                string = "case5-nginx-pod-1"
-	case5PodName2                string = "case5-nginx-pod-2"
-	case5InformYaml              string = "../resources/case5_multi/case5_multi_mh.yaml"
-	case5EnforceYaml             string = "../resources/case5_multi/case5_multi_enforce.yaml"
-	case5ComboYaml               string = "../resources/case5_multi/case5_multi_combo.yaml"
-)
-
 var _ = Describe("Test multiple obj template handling", Ordered, func() {
+	const (
+		configPolicyNameInform  string = "policy-pod-multi-mh"
+		configPolicyNameEnforce string = "policy-pod-multi-create"
+		configPolicyNameCombo   string = "policy-pod-multi-combo"
+		podName1                string = "case5-nginx-pod-1"
+		podName2                string = "case5-nginx-pod-2"
+		informYaml              string = "../resources/case5_multi/case5_multi_mh.yaml"
+		enforceYaml             string = "../resources/case5_multi/case5_multi_enforce.yaml"
+		comboYaml               string = "../resources/case5_multi/case5_multi_combo.yaml"
+		singleYaml              string = "../resources/case5_multi/case5_single_mh.yaml"
+	)
+
 	Describe("Create a policy on managed cluster in ns:"+testNamespace, Ordered, func() {
 		It("should be created properly on the managed cluster", func() {
-			By("Creating " + case5ConfigPolicyNameInform + " and " + case5ConfigPolicyNameCombo + " on managed")
-			utils.Kubectl("apply", "-f", case5InformYaml, "-n", testNamespace)
+			By("Creating " + configPolicyNameInform + " and " + configPolicyNameCombo + " on managed")
+			utils.Kubectl("apply", "-f", informYaml, "-n", testNamespace)
 			plc := utils.GetWithTimeout(clientManagedDynamic, gvrConfigPolicy,
-				case5ConfigPolicyNameInform, testNamespace, true, defaultTimeoutSeconds)
+				configPolicyNameInform, testNamespace, true, defaultTimeoutSeconds)
 			Expect(plc).NotTo(BeNil())
 			Eventually(func(g Gomega) {
 				managedPlc := utils.GetWithTimeout(clientManagedDynamic, gvrConfigPolicy,
-					case5ConfigPolicyNameInform, testNamespace, true, defaultTimeoutSeconds)
+					configPolicyNameInform, testNamespace, true, defaultTimeoutSeconds)
 
 				utils.CheckComplianceStatus(g, managedPlc, "NonCompliant")
 			}, defaultTimeoutSeconds, 1).Should(Succeed())
-			utils.Kubectl("apply", "-f", case5ComboYaml, "-n", testNamespace)
+			utils.Kubectl("apply", "-f", comboYaml, "-n", testNamespace)
 			plc = utils.GetWithTimeout(clientManagedDynamic, gvrConfigPolicy,
-				case5ConfigPolicyNameCombo, testNamespace, true, defaultTimeoutSeconds)
+				configPolicyNameCombo, testNamespace, true, defaultTimeoutSeconds)
 			Expect(plc).NotTo(BeNil())
 			Eventually(func(g Gomega) {
 				managedPlc := utils.GetWithTimeout(clientManagedDynamic, gvrConfigPolicy,
-					case5ConfigPolicyNameCombo, testNamespace, true, defaultTimeoutSeconds)
+					configPolicyNameCombo, testNamespace, true, defaultTimeoutSeconds)
 
 				utils.CheckComplianceStatus(g, managedPlc, "NonCompliant")
 			}, defaultTimeoutSeconds, 1).Should(Succeed())
 		})
 		It("should create pods on managed cluster", func() {
-			By("creating " + case5ConfigPolicyNameEnforce + " on hub with spec.remediationAction = enforce")
-			utils.Kubectl("apply", "-f", case5EnforceYaml, "-n", testNamespace)
+			By("creating " + configPolicyNameEnforce + " on hub with spec.remediationAction = enforce")
+			utils.Kubectl("apply", "-f", enforceYaml, "-n", testNamespace)
 			plc := utils.GetWithTimeout(clientManagedDynamic, gvrConfigPolicy,
-				case5ConfigPolicyNameEnforce, testNamespace, true, defaultTimeoutSeconds)
+				configPolicyNameEnforce, testNamespace, true, defaultTimeoutSeconds)
 			Expect(plc).NotTo(BeNil())
 			Eventually(func(g Gomega) {
 				managedPlc := utils.GetWithTimeout(clientManagedDynamic, gvrConfigPolicy,
-					case5ConfigPolicyNameEnforce, testNamespace, true, defaultTimeoutSeconds)
+					configPolicyNameEnforce, testNamespace, true, defaultTimeoutSeconds)
 
 				utils.CheckComplianceStatus(g, managedPlc, "Compliant")
 			}, defaultTimeoutSeconds, 1).Should(Succeed())
 			Eventually(func(g Gomega) {
 				informPlc := utils.GetWithTimeout(clientManagedDynamic, gvrConfigPolicy,
-					case5ConfigPolicyNameInform, testNamespace, true, defaultTimeoutSeconds)
+					configPolicyNameInform, testNamespace, true, defaultTimeoutSeconds)
 
 				utils.CheckComplianceStatus(g, informPlc, "Compliant")
 			}, defaultTimeoutSeconds, 1).Should(Succeed())
 			Eventually(func(g Gomega) {
 				comboPlc := utils.GetWithTimeout(clientManagedDynamic, gvrConfigPolicy,
-					case5ConfigPolicyNameCombo, testNamespace, true, defaultTimeoutSeconds)
+					configPolicyNameCombo, testNamespace, true, defaultTimeoutSeconds)
 
 				utils.CheckComplianceStatus(g, comboPlc, "NonCompliant")
 			}, defaultTimeoutSeconds, 1).Should(Succeed())
 			pod1 := utils.GetWithTimeout(clientManagedDynamic, gvrPod,
-				case5PodName1, "default", true, defaultTimeoutSeconds)
+				podName1, "default", true, defaultTimeoutSeconds)
 			Expect(pod1).NotTo(BeNil())
 			pod2 := utils.GetWithTimeout(clientManagedDynamic, gvrPod,
-				case5PodName2, "default", true, defaultTimeoutSeconds)
+				podName2, "default", true, defaultTimeoutSeconds)
 			Expect(pod2).NotTo(BeNil())
+		})
+		It("should only have compliancy details on the current objects when templates are removed", func() {
+			By("confirming the current details on the policy")
+			Eventually(func(g Gomega) {
+				managedPlc := utils.GetWithTimeout(clientManagedDynamic, gvrConfigPolicy,
+					configPolicyNameInform, testNamespace, true, defaultTimeoutSeconds)
+				details, _, _ := unstructured.NestedSlice(managedPlc.Object, "status", "compliancyDetails")
+				g.Expect(details).To(HaveLen(2))
+			}, defaultTimeoutSeconds, 1).Should(Succeed())
+
+			By("removing the second template on the policy")
+			utils.Kubectl("apply", "-f", singleYaml, "-n", testNamespace)
+
+			By("checking the new details on the policy")
+			Eventually(func(g Gomega) {
+				managedPlc := utils.GetWithTimeout(clientManagedDynamic, gvrConfigPolicy,
+					configPolicyNameInform, testNamespace, true, defaultTimeoutSeconds)
+				details, _, _ := unstructured.NestedSlice(managedPlc.Object, "status", "compliancyDetails")
+				g.Expect(details).To(HaveLen(1))
+			}, defaultTimeoutSeconds, 1).Should(Succeed())
 		})
 		AfterAll(func() {
 			policies := []string{
-				case5ConfigPolicyNameInform,
-				case5ConfigPolicyNameEnforce,
-				case5ConfigPolicyNameCombo,
+				configPolicyNameInform,
+				configPolicyNameEnforce,
+				configPolicyNameCombo,
 			}
 
 			deleteConfigPolicies(policies)
 
 			By("Delete pods")
-			pods := []string{case5PodName1, case5PodName2}
+			pods := []string{podName1, podName2}
 			namespaces := []string{"default"}
 			deletePods(pods, namespaces)
 		})
