0e6ef2949be8ae67429416c4a987489656ece571
Justin Kulikauskas <jkulikau@redhat.com>
2024-10-16 22:53:30 -0400

    Handle SCC annotations in namespaces
    Several annotations relating to SCCs are automatically populated on
    namespaces by OpenShift when they are empty. Previously, applying
    annotations to a namespace via a mustonlyhave policy was then causing
    those values to be removed and then be set again, interfering with
    workloads in that namespace. Now these annotations are specially handled
    to avoid this. SCC annotations set explicitly in the policy will still
    be checked correctly.
    
    Refs:
     - https://issues.redhat.com/browse/ACM-12507
    
    Signed-off-by: Justin Kulikauskas <jkulikau@redhat.com>

--- test/e2e/case9_md_check_test.go
+++ test/e2e/case9_md_check_test.go
@@ -10,32 +10,32 @@ import (
 	"open-cluster-management.io/config-policy-controller/test/utils"
 )
 
-const (
-	case9ConfigPolicyNamePod           string = "policy-pod-c9-create"
-	case9ConfigPolicyNameAnno          string = "policy-pod-anno"
-	case9ConfigPolicyNameNoAnno        string = "policy-pod-no-anno"
-	case9ConfigPolicyNameLabelPatch    string = "policy-label-patch"
-	case9ConfigPolicyNameLabelCheck    string = "policy-label-check"
-	case9ConfigPolicyNameLabelAuto     string = "policy-label-check-auto"
-	case9ConfigPolicyNameNSCreate      string = "policy-c9-create-ns"
-	case9ConfigPolicyNameIgnoreLabels  string = "policy-ignore-labels"
-	case9MultiAnnoNSCreate             string = "policy-create-ns-multiple-annotations"
-	case9CheckNSMusthave               string = "policy-check-ns-mdcomptype-mh"
-	case9CheckNSMustonlyhave           string = "policy-check-ns-mdcomptype-moh"
-	case9PolicyYamlPod                 string = "../resources/case9_md_check/case9_pod_create.yaml"
-	case9PolicyYamlAnno                string = "../resources/case9_md_check/case9_annos.yaml"
-	case9PolicyYamlNoAnno              string = "../resources/case9_md_check/case9_no_annos.yaml"
-	case9PolicyYamlLabelPatch          string = "../resources/case9_md_check/case9_label_patch.yaml"
-	case9PolicyYamlLabelCheck          string = "../resources/case9_md_check/case9_label_check.yaml"
-	case9PolicyYamlLabelAuto           string = "../resources/case9_md_check/case9_label_check_auto.yaml"
-	case9PolicyYamlIgnoreLabels        string = "../resources/case9_md_check/case9_mustonlyhave_nolabels.yaml"
-	case9PolicyYamlNSCreate            string = "../resources/case9_md_check/case9_ns_create.yaml"
-	case9PolicyYamlMultiAnnoNSCreate   string = "../resources/case9_md_check/case9_multianno_ns_create.yaml"
-	case9PolicyYamlCheckNSMusthave     string = "../resources/case9_md_check/case9_checkns-md-mh.yaml"
-	case9PolicyYamlCheckNSMustonlyhave string = "../resources/case9_md_check/case9_checkns-md-moh.yaml"
-)
-
 var _ = Describe("Test pod obj template handling", func() {
+	const (
+		case9ConfigPolicyNamePod           string = "policy-pod-c9-create"
+		case9ConfigPolicyNameAnno          string = "policy-pod-anno"
+		case9ConfigPolicyNameNoAnno        string = "policy-pod-no-anno"
+		case9ConfigPolicyNameLabelPatch    string = "policy-label-patch"
+		case9ConfigPolicyNameLabelCheck    string = "policy-label-check"
+		case9ConfigPolicyNameLabelAuto     string = "policy-label-check-auto"
+		case9ConfigPolicyNameNSCreate      string = "policy-c9-create-ns"
+		case9ConfigPolicyNameIgnoreLabels  string = "policy-ignore-labels"
+		case9MultiAnnoNSCreate             string = "policy-create-ns-multiple-annotations"
+		case9CheckNSMusthave               string = "policy-check-ns-mdcomptype-mh"
+		case9CheckNSMustonlyhave           string = "policy-check-ns-mdcomptype-moh"
+		case9PolicyYamlPod                 string = "../resources/case9_md_check/case9_pod_create.yaml"
+		case9PolicyYamlAnno                string = "../resources/case9_md_check/case9_annos.yaml"
+		case9PolicyYamlNoAnno              string = "../resources/case9_md_check/case9_no_annos.yaml"
+		case9PolicyYamlLabelPatch          string = "../resources/case9_md_check/case9_label_patch.yaml"
+		case9PolicyYamlLabelCheck          string = "../resources/case9_md_check/case9_label_check.yaml"
+		case9PolicyYamlLabelAuto           string = "../resources/case9_md_check/case9_label_check_auto.yaml"
+		case9PolicyYamlIgnoreLabels        string = "../resources/case9_md_check/case9_mustonlyhave_nolabels.yaml"
+		case9PolicyYamlNSCreate            string = "../resources/case9_md_check/case9_ns_create.yaml"
+		case9PolicyYamlMultiAnnoNSCreate   string = "../resources/case9_md_check/case9_multianno_ns_create.yaml"
+		case9PolicyYamlCheckNSMusthave     string = "../resources/case9_md_check/case9_checkns-md-mh.yaml"
+		case9PolicyYamlCheckNSMustonlyhave string = "../resources/case9_md_check/case9_checkns-md-moh.yaml"
+	)
+
 	Describe("Create a pod policy on managed cluster in ns:"+testNamespace, Ordered, func() {
 		It("should create a policy properly on the managed cluster", func() {
 			By("Creating " + case9ConfigPolicyNamePod + " on managed")
@@ -126,6 +126,30 @@ var _ = Describe("Test pod obj template handling", func() {
 					case9ConfigPolicyNameNSCreate, testNamespace, true, defaultTimeoutSeconds)
 
 				utils.CheckComplianceStatus(g, managedPlc, "Compliant")
+			}, defaultTimeoutSeconds, 1).Should(Succeed())
+		})
+		It("should not remove scc namespace annotations even in mustonlyhave mode", func() {
+			By("Checking the current annotations")
+			obj := utils.GetWithTimeout(clientManagedDynamic, gvrNS,
+				"case9-test", "", true, defaultTimeoutSeconds)
+			Expect(obj.GetAnnotations()).To(HaveKeyWithValue("foo.bar/baz", "hello world"))
+			Expect(obj.GetAnnotations()).To(HaveKeyWithValue("openshift.io/sa.scc.policy", "keep"))
+
+			By("Patching the annotations on the namespace")
+			utils.Kubectl("patch", "namespace", "case9-test", "-o=yaml", "--type=merge",
+				`-p={"metadata":{"annotations":{`+
+					`"openshift.io/sa.scc.test": "example",`+
+					`"openshift.io/sa.scc.policy": "example",`+
+					`"foo.bar/baz": "incorrect"}}}`)
+
+			By("Verifying the annotations in the policy are updated, and the new SCC annotation is kept")
+			Eventually(func(g Gomega) {
+				utils.Kubectl("get", "namespace", "case9-test", "-o=yaml")
+				obj := utils.GetWithTimeout(clientManagedDynamic, gvrNS,
+					"case9-test", "", true, defaultTimeoutSeconds)
+				g.Expect(obj.GetAnnotations()).To(HaveKeyWithValue("foo.bar/baz", "hello world"))
+				g.Expect(obj.GetAnnotations()).To(HaveKeyWithValue("openshift.io/sa.scc.test", "example"))
+				g.Expect(obj.GetAnnotations()).To(HaveKeyWithValue("openshift.io/sa.scc.policy", "keep"))
 			}, defaultTimeoutSeconds, 1).Should(Succeed())
 		})
 		It("should ignore labels and annotations if none are specified in the template", func() {
