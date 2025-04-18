f4c8d8ed756faf749dbef3deb3b997b519423ac4
Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>
2025-04-18 15:40:59 -0400

    Move template context vars to new test file
    Signed-off-by: Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>

---
+++ test/e2e/case44_template_vars_test.go
@@ -0,0 +1,349 @@
+// Copyright (c) 2020 Red Hat, Inc.
+// Copyright Contributors to the Open Cluster Management project
+
+package e2e
+
+import (
+	"fmt"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+
+	"open-cluster-management.io/config-policy-controller/test/utils"
+)
+
+var _ = Describe("Test template context variables", Ordered, func() {
+	const (
+		rsrcPath string = "../resources/case44_template_vars/"
+	)
+
+	Describe("Policy with the ObjectNamespace variables", Ordered, func() {
+		const (
+			preReqs = rsrcPath + "case44_objectns_var_prereqs.yaml"
+		)
+
+		BeforeEach(func() {
+			By("Creating the prerequisites")
+			utils.Kubectl("apply", "-f", preReqs)
+		})
+
+		AfterEach(func() {
+			utils.KubectlDelete("configurationpolicy", "-l=e2e-test=case44", "-n", testNamespace)
+		})
+
+		AfterAll(func() {
+			utils.KubectlDelete("-f", preReqs)
+		})
+
+		It("Should enforce the labels on the e2e-object-variable ConfigMaps", func(ctx SpecContext) {
+			const (
+				policyName = "case44-object-variables"
+			)
+
+			By("Applying the " + policyName + " ConfigurationPolicy")
+			utils.Kubectl("apply", "-n", testNamespace, "-f", rsrcPath+"case44_objectns_var.yaml")
+
+			By("By verifying that the ConfigurationPolicy is compliant and has the correct related objects")
+			Eventually(func(g Gomega) {
+				managedPlc := utils.GetWithTimeout(
+					clientManagedDynamic, gvrConfigPolicy, policyName, testNamespace, true, defaultTimeoutSeconds,
+				)
+
+				utils.CheckComplianceStatus(g, managedPlc, "Compliant")
+
+				relatedObjects, _, _ := unstructured.NestedSlice(managedPlc.Object, "status", "relatedObjects")
+				g.Expect(relatedObjects).To(HaveLen(2))
+
+				relatedObject1, ok := relatedObjects[0].(map[string]interface{})
+				g.Expect(ok).To(BeTrue(), "Related object is not a map")
+
+				relatedObject1NS, _, _ := unstructured.NestedString(relatedObject1, "object", "metadata", "namespace")
+				g.Expect(relatedObject1NS).To(
+					Equal("case44-e2e-object-variables"), "Related object namespace should match",
+				)
+
+				relatedObject2, ok := relatedObjects[1].(map[string]interface{})
+				g.Expect(ok).To(BeTrue(), "Related object is not a map")
+
+				relatedObject2NS, _, _ := unstructured.NestedString(relatedObject2, "object", "metadata", "namespace")
+				g.Expect(relatedObject2NS).To(Equal("default"), "Related object namespace should match")
+			}, defaultTimeoutSeconds, 1).Should(Succeed())
+
+			By("By verifying the ConfigMaps")
+			configMap1, err := clientManaged.CoreV1().ConfigMaps("case44-e2e-object-variables").Get(
+				ctx, "case44-e2e-object-variables", metav1.GetOptions{},
+			)
+			Expect(err).ToNot(HaveOccurred())
+
+			Expect(configMap1.ObjectMeta.Labels).To(HaveKeyWithValue("case44", "passed"))
+			Expect(configMap1.ObjectMeta.Labels).To(HaveKeyWithValue("namespace", "case44-e2e-object-variables"))
+
+			configMap2, err := clientManaged.CoreV1().ConfigMaps("default").Get(
+				ctx, "case44-e2e-object-variables", metav1.GetOptions{},
+			)
+			Expect(err).ToNot(HaveOccurred())
+
+			Expect(configMap2.ObjectMeta.Labels).To(HaveKeyWithValue("case44", "passed"))
+			Expect(configMap2.ObjectMeta.Labels).To(HaveKeyWithValue("namespace", "default"))
+		})
+
+		It("Should fail when the namespace doesn't match after template resolution", func() {
+			const (
+				invalidPolicyName = "case44-invalid-ns"
+			)
+
+			By("Applying the " + invalidPolicyName + " ConfigurationPolicy")
+			utils.Kubectl("apply", "-n", testNamespace, "-f", rsrcPath+"case44_objectns_var_invalid_ns.yaml")
+
+			By("By verifying that the ConfigurationPolicy is noncompliant")
+			Eventually(func(g Gomega) {
+				managedPlc := utils.GetWithTimeout(
+					clientManagedDynamic,
+					gvrConfigPolicy,
+					invalidPolicyName,
+					testNamespace,
+					true,
+					defaultTimeoutSeconds,
+				)
+
+				utils.CheckComplianceStatus(g, managedPlc, "NonCompliant")
+				g.Expect(utils.GetStatusMessage(managedPlc)).To(Equal(
+					"The object definition's namespace must match the result " +
+						"from the namespace selector after template resolution",
+				))
+			}, defaultTimeoutSeconds, 1).Should(Succeed())
+		})
+	})
+
+	Describe("Policy with the ObjectName variable", Ordered, func() {
+		const (
+			preReqs     = rsrcPath + "case44_objectname_var_prereqs.yaml"
+			e2eBaseName = "case44-e2e-objectname-var"
+		)
+
+		BeforeEach(func() {
+			By("Creating the prerequisites")
+			utils.Kubectl("apply", "-f", preReqs)
+		})
+
+		AfterEach(func() {
+			utils.KubectlDelete("configurationpolicy", "-l=e2e-test=case44", "-n", testNamespace)
+			utils.KubectlDelete("configmaps", "-n", e2eBaseName, "-l=e2e-test=case44")
+		})
+
+		AfterAll(func() {
+			utils.KubectlDelete("-f", preReqs)
+		})
+
+		It("Should enforce the labels on the case44-e2e-objectname-var* ConfigMaps", func(ctx SpecContext) {
+			const (
+				policyName = "case44-objectname-var"
+			)
+			By("Applying the " + policyName + " ConfigurationPolicy")
+			utils.Kubectl("apply", "-n", testNamespace, "-f", rsrcPath+"case44_objectname_var.yaml")
+
+			By("By verifying that the ConfigurationPolicy is compliant and has the correct related objects")
+			Eventually(func(g Gomega) {
+				managedPlc := utils.GetWithTimeout(
+					clientManagedDynamic, gvrConfigPolicy, policyName, testNamespace, true, defaultTimeoutSeconds,
+				)
+
+				utils.CheckComplianceStatus(g, managedPlc, "Compliant")
+				g.Expect(utils.GetStatusMessage(managedPlc)).Should(Equal(fmt.Sprintf(
+					"configmaps [case44-e2e-objectname-var2, case44-e2e-objectname-var3] "+
+						"found as specified in namespace %[1]s",
+					e2eBaseName)))
+
+				relatedObjects, _, _ := unstructured.NestedSlice(managedPlc.Object, "status", "relatedObjects")
+				g.Expect(relatedObjects).To(HaveLen(2))
+
+				for idx := range relatedObjects {
+					relatedObject, ok := relatedObjects[idx].(map[string]interface{})
+					g.Expect(ok).To(BeTrue(), "Related object is not a map")
+					relatedObject1NS, _, _ := unstructured.NestedString(relatedObject, "object", "metadata", "name")
+					// The first object is skipped.
+					g.Expect(relatedObject1NS).To(
+						Equal(fmt.Sprintf("%s%d", e2eBaseName, idx+2)),
+						"Related object name should match")
+				}
+			}, defaultTimeoutSeconds, 1).Should(Succeed())
+
+			By("By verifying the ConfigMaps")
+			configMaps, err := clientManaged.CoreV1().ConfigMaps(e2eBaseName).List(
+				ctx, metav1.ListOptions{
+					LabelSelector: "case44",
+				},
+			)
+			Expect(err).ToNot(HaveOccurred())
+
+			for _, cm := range configMaps.Items {
+				if cm.Name == "case44-e2e-objectname-var1" {
+					continue
+				}
+
+				Expect(cm.ObjectMeta.Labels).To(HaveKeyWithValue("case44", "passed"))
+				Expect(cm.ObjectMeta.Labels).To(HaveKeyWithValue("name", cm.GetName()))
+				Expect(cm.ObjectMeta.Labels).To(HaveKeyWithValue("namespace", cm.GetNamespace()))
+			}
+		})
+
+		DescribeTable("Should succeed when context vars are in use but name/namespace are left empty",
+			func(ctx SpecContext, policyName string, policyYAML string) {
+				By("Applying the " + policyName + " ConfigurationPolicy")
+				utils.Kubectl("apply", "-n", testNamespace, "-f", rsrcPath+policyYAML)
+
+				By("By verifying that the ConfigurationPolicy is compliant and has the correct related objects")
+				Eventually(func(g Gomega) {
+					managedPlc := utils.GetWithTimeout(
+						clientManagedDynamic,
+						gvrConfigPolicy,
+						policyName,
+						testNamespace,
+						true,
+						defaultTimeoutSeconds,
+					)
+
+					utils.CheckComplianceStatus(g, managedPlc, "Compliant")
+					g.Expect(utils.GetStatusMessage(managedPlc)).Should(
+						Equal("configmaps [case44-e2e-objectname-var3] found as specified in namespace " + e2eBaseName))
+
+					relatedObjects, _, _ := unstructured.NestedSlice(managedPlc.Object, "status", "relatedObjects")
+					g.Expect(relatedObjects).To(HaveLen(1))
+					relatedObject, ok := relatedObjects[0].(map[string]interface{})
+					g.Expect(ok).To(BeTrue(), "Related object is not a map")
+					relatedObject1NS, _, _ := unstructured.NestedString(relatedObject, "object", "metadata", "name")
+					g.Expect(relatedObject1NS).To(
+						Equal(fmt.Sprintf("%s%d", e2eBaseName, 3)),
+						"Related object name should match")
+				}, defaultTimeoutSeconds, 1).Should(Succeed())
+
+				By("By verifying the ConfigMaps")
+				cm, err := clientManaged.CoreV1().ConfigMaps(e2eBaseName).Get(
+					ctx, "case44-e2e-objectname-var3", metav1.GetOptions{})
+				Expect(err).ToNot(HaveOccurred())
+				Expect(cm.ObjectMeta.Labels).To(HaveKeyWithValue("case44", "passed"))
+				Expect(cm.ObjectMeta.Labels).To(HaveKeyWithValue("object-name", cm.GetName()))
+				Expect(cm.ObjectMeta.Labels).To(HaveKeyWithValue("object-namespace", cm.GetNamespace()))
+			},
+			Entry("inside an if conditional", "case44-outside-name", "case44_objectname_var_outside_name.yaml"),
+			Entry("with a boolean argument", "case44-outside-name-arg", "case44_objectname_var_outside_name_arg.yaml"),
+		)
+
+		It("Should fail when the name doesn't match after template resolution", func() {
+			const (
+				invalidPolicyName = "case44-invalid-name"
+			)
+
+			By("Applying the " + invalidPolicyName + " ConfigurationPolicy")
+			utils.Kubectl("apply", "-n", testNamespace, "-f", rsrcPath+"case44_objectname_var_invalid_name.yaml")
+
+			By("By verifying that the ConfigurationPolicy is noncompliant")
+			Eventually(func(g Gomega) {
+				managedPlc := utils.GetWithTimeout(
+					clientManagedDynamic,
+					gvrConfigPolicy,
+					invalidPolicyName,
+					testNamespace,
+					true,
+					defaultTimeoutSeconds,
+				)
+
+				utils.CheckComplianceStatus(g, managedPlc, "NonCompliant")
+				g.Expect(utils.GetStatusMessage(managedPlc)).To(Equal(
+					"The object definition's name must match the result " +
+						"from the object selector after template resolution",
+				))
+			}, defaultTimeoutSeconds, 1).Should(Succeed())
+		})
+
+		DescribeTable("Should fail when skipObject is called",
+			func(policyName string, policyYAML string, errString string) {
+				By("Applying the " + policyName + " ConfigurationPolicy")
+				utils.Kubectl("apply", "-n", testNamespace, "-f", rsrcPath+policyYAML)
+
+				By("By verifying that the ConfigurationPolicy is noncompliant")
+				Eventually(func(g Gomega) {
+					managedPlc := utils.GetWithTimeout(
+						clientManagedDynamic,
+						gvrConfigPolicy,
+						policyName,
+						testNamespace,
+						true,
+						defaultTimeoutSeconds,
+					)
+
+					utils.CheckComplianceStatus(g, managedPlc, "NonCompliant")
+					g.Expect(utils.GetStatusMessage(managedPlc)).To(HaveSuffix(
+						"error calling skipObject: " + errString))
+				}, defaultTimeoutSeconds, 1).Should(Succeed())
+			},
+			Entry("with multiple arguments", "case44-skipobject-multi-arg", "case44_skipobject_multi_arg.yaml",
+				`expected one optional boolean argument but received 3 arguments`),
+			Entry("with a non-boolean argument", "case44-skipobject-non-bool", "case44_skipobject_non_bool.yaml",
+				`expected boolean but received 'not a boolean'`),
+		)
+
+		It("Should fail when context variables are used without a selector", func() {
+			for name, test := range map[string]struct {
+				file, expectedStruct string
+			}{
+				"case44-no-selector": {
+					file:           "case44_objectname_var_noselector.yaml",
+					expectedStruct: " ObjectNamespace string ",
+				},
+				"case44-no-selector-ns": {
+					file: "case44_objectname_var_noselector_ns.yaml",
+				},
+			} {
+				By("Applying the " + name + " ConfigurationPolicy")
+				utils.Kubectl("apply", "-n", testNamespace, "-f", rsrcPath+test.file)
+
+				By("By verifying that the ConfigurationPolicy is noncompliant")
+				Eventually(func(g Gomega) {
+					managedPlc := utils.GetWithTimeout(
+						clientManagedDynamic,
+						gvrConfigPolicy,
+						name,
+						testNamespace,
+						true,
+						defaultTimeoutSeconds,
+					)
+
+					utils.CheckComplianceStatus(g, managedPlc, "NonCompliant")
+					g.Expect(utils.GetStatusMessage(managedPlc)).To(ContainSubstring(
+						`template: tmpl:6:14: executing "tmpl" at <.ObjectName>: ` +
+							`can't evaluate field ObjectName in type struct {` + test.expectedStruct + `}`,
+					))
+				}, defaultTimeoutSeconds, 1).Should(Succeed())
+			}
+		})
+
+		It("Should be compliant when all objects are skipped with skipObject", func() {
+			const (
+				allSkippedPolicyName = "case44-objectname-var-all-skipped"
+			)
+
+			By("Applying the " + allSkippedPolicyName + " ConfigurationPolicy")
+			utils.Kubectl("apply", "-n", testNamespace, "-f", rsrcPath+"case44_objectname_var_all_skipped.yaml")
+
+			By("By verifying that the ConfigurationPolicy is compliant")
+			Eventually(func(g Gomega) {
+				managedPlc := utils.GetWithTimeout(
+					clientManagedDynamic,
+					gvrConfigPolicy,
+					allSkippedPolicyName,
+					testNamespace,
+					true,
+					defaultTimeoutSeconds,
+				)
+
+				utils.CheckComplianceStatus(g, managedPlc, "Compliant")
+				g.Expect(utils.GetStatusMessage(managedPlc)).To(Equal(
+					"All objects of kind ConfigMap were skipped by the `skipObject` template function",
+				))
+			}, defaultTimeoutSeconds, 1).Should(Succeed())
+		})
+	})
+})
