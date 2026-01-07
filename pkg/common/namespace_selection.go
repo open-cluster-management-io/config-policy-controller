31ce640480758d260b7631df01be4ed224f7d699
Justin Kulikauskas <jkulikau@redhat.com>
2026-01-07 14:10:01 -0500

    Refactor namespace filtering
    Previously, the `Get` method was duplicating some of what is done in the
    `filter` function. This allowed it to use a more targeted List API call,
    but it's not clear if that would actually improve performance.
    
    Now, all namespace filtering logic is handled by the `filter` function,
    which takes a list of all the namespaces. That list will definitely be
    cached by controller-runtime, and it simplifies the flow. Additionally,
    a comment typo has been corrected, and `maps.Copy()` is used instead of
    a manual loop.
    
    Signed-off-by: Justin Kulikauskas <jkulikau@redhat.com>

--- pkg/common/namespace_selection.go
+++ pkg/common/namespace_selection.go
@@ -6,6 +6,7 @@ package common
 import (
 	"context"
 	"fmt"
+	"maps"
 	"reflect"
 	"slices"
 	"strings"
@@ -98,11 +99,7 @@ func (r *NamespaceSelectorReconciler) Reconcile(ctx context.Context, _ ctrl.Requ
 	oldSelections := make(map[string]namespaceSelection)
 
 	r.lock.RLock()
-
-	for name, selection := range r.selections {
-		oldSelections[name] = selection
-	}
-
+	maps.Copy(oldSelections, r.selections)
 	r.lock.RUnlock()
 
 	// No selections to populate, just skip.
@@ -157,7 +154,7 @@ func (r *NamespaceSelectorReconciler) Reconcile(ctx context.Context, _ ctrl.Requ
 }
 
 // Get returns the items matching the given Target for the given object. If there's no selection for that object,
-// and Target has been calculated, it will be calculated now. Otherwise, a cached value may be used.
+// and Target has not been calculated, it will be calculated now. Otherwise, a cached value may be used.
 func (r *NamespaceSelectorReconciler) Get(objNS string, objName string, t policyv1.Target) ([]string, error) {
 	log := logf.Log.WithValues("Reconciler", "NamespaceSelector")
 
@@ -193,49 +190,19 @@ func (r *NamespaceSelectorReconciler) Get(objNS string, objName string, t policy
 		return []string{}, nil
 	}
 
-	// New, or the target has changed.
 	nsList := corev1.NamespaceList{}
-	// Default to fetching all Namespaces
-	listOpts := client.ListOptions{
-		LabelSelector: labels.Everything(),
-	}
-
-	// Parse the label selector if it's provided
-	if t.LabelSelector != nil {
-		selector, err := metav1.LabelSelectorAsSelector(t.LabelSelector)
-		if err != nil {
-			err = fmt.Errorf("error parsing namespace LabelSelector: %w", err)
-
-			log.V(2).Info("Updating selection from Reconcile with parsing error",
-				"namespace", objNS, "policy", objName, "error", err)
-
-			r.update(objNS, objName, namespaceSelection{
-				target:     t,
-				namespaces: []string{},
-				hasUpdate:  false,
-				err:        err,
-			})
-
-			return []string{}, fmt.Errorf("error parsing namespace LabelSelector: %w", err)
-		}
-
-		listOpts.LabelSelector = selector
-	}
 
 	// Fetch namespaces -- this List will be from the controller-runtime cache
-	if err := r.client.List(context.TODO(), &nsList, &listOpts); err != nil {
+	if err := r.client.List(context.TODO(), &nsList); err != nil {
 		log.Error(err, "Unable to list namespaces from the cache")
 
 		return nil, err
 	}
 
-	nsToMatch := make([]string, len(nsList.Items))
-	for i, ns := range nsList.Items {
-		nsToMatch[i] = ns.Name
+	selected, err := filter(nsList, t)
+	if err != nil {
+		log.Error(err, "Unable to filter namespaces for policy", "namespace", objNS, "policy", objName)
 	}
-
-	selected, err := Matches(nsToMatch, t.Include, t.Exclude)
-	slices.Sort(selected)
 
 	log.V(2).Info("Updating selection from Reconcile with matches",
 		"namespace", objNS, "policy", objName, "selection", selected, "error", err)
